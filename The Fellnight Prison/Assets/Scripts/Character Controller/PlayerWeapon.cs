e95bb49476bacafe8e1db27a788810794cfb086d
Hypno-cube <logan.c.pettit@gmail.com>
2015-10-22 07:07:47 -0500

    Character Controler import
    IT'S SO CLOSE TO BEING A GAME DUDE

---
+++ The Fellnight Prison/Assets/Scripts/Character Controller/PlayerWeapon.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerWeapon : MonoBehaviour
+{
+
+    public GameObject weapon;
+    private BoxCollider sword;
+
+    // Use this for initialization
+    void Start()
+    {
+        sword = weapon.GetComponent<BoxCollider>();
+
+
+    }
+    void OnCollisionEnter(Collision sword)
+    {
+        if (sword.gameObject.name == "target")
+        {
+
+            Debug.Log("you hit a target");
+            Destroy(sword.gameObject);
+
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ The Fellnight Prison/Assets/Scripts/Character Controller/Swing.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swing : MonoBehaviour
+{
+    public Camera cam;
+    public Animator anim;
+    private float inputX;
+    private float inputY;
+    private float Run;
+    private float inputH;
+
+    private bool isRunning = false;
+
+    public GameObject Weapon;
+    private BoxCollider sword;
+
+    public Animation clip;
+
+    public float moveSpeed = 5f;
+    public float MS = 5f;
+
+    float VR = 0;
+    public float UDR = 60f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        sword = Weapon.GetComponent<BoxCollider>();
+        clip = GetComponent<Animation>();
+        //Time.timeScale = 0.5F;
+
+    }
+
+    public int frame = 0;
+    public int runFrames = 25;
+
+    IEnumerator colliderOn()
+    {
+        while (frame < runFrames)
+        {
+            frame++;
+            //print(frame);
+            // Debug.Log("time into animation");
+            sword.enabled = true;
+            MS = .5f;
+            yield return null;
+        }
+        sword.enabled = false;
+        frame = 0;
+        MS = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Handles camera and player rotation
+        float rotLR = Input.GetAxis("Mouse X") * MS;
+        transform.Rotate(0, rotLR, 0);
+        VR -= Input.GetAxis("Mouse Y") * MS;
+        VR = Mathf.Clamp(VR, -UDR, UDR);
+        Camera.main.transform.localRotation = Quaternion.Euler(VR, 0, 0);
+
+
+        //Gets the mouse direction and saves it to inputX/inputY
+        inputY = Input.GetAxis("Mouse Y");
+        inputX = Input.GetAxis("Mouse X");
+        //Then sets the animation paramiter float to that value
+        anim.SetFloat("posX", inputX);
+        anim.SetFloat("Ydirection", inputY);
+
+
+        //Same as above but for movement
+        Run = Input.GetAxis("Vertical");
+        inputH = Input.GetAxis("Horizontal");
+        //Run is forward and input H is sideways
+        anim.SetFloat("Run", Run);
+        anim.SetFloat("inputH", inputH);
+
+
+        //Hold down leftshit to sprint
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isRunning = true;
+            anim.SetBool("isRunning", true);
+            Debug.Log("shift being held down");
+        }
+        else
+        {
+            isRunning = false;
+            anim.SetBool("isRunning", false);
+        }
+
+
+        //press right mouse button to block and let go to stop blocking
+        if (Input.GetMouseButtonDown(1))
+        {
+            anim.SetBool("isBlocking", true);
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            anim.SetBool("isBlocking", false);
+        }
+
+
+        //Handles player attacking
+        //if player clicks left mouse button and moves in a direction an attack will occur from that direction
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // if (inputH != 0 || inputY != 0)
+            //{
+            anim.SetTrigger("Attack");
+            StartCoroutine("colliderOn");
+            // }
+        }
+
+
+        /*
+        if (Input.GetButtonDown("Fire1") && (Input.GetAxis("Mouse X") == -1))
+        {
+                //play animation for left move
+                anim.Play("Attack2", -1, 0f);
+        }
+        if (Input.GetButtonDown("Fire1") && (Input.GetAxis("Mouse X") == 1))
+        {
+                //play animation for right move
+                anim.Play("Attack1", -1, 0f);
+        }
+        */
+
+    }
+}
