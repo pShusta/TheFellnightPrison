ccc7900a597411eb2303e08366468c8afc0ff4a6
Phillip Shusta <pshusta@gmail.com>
2015-10-24 05:46:50 -0500

    SKELETON in dungeon, shrink skeleton prefab and start or anim sync

---
+++ The Fellnight Prison/Assets/Scripts/Networking/NetworkMotion.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkMotion : Photon.MonoBehaviour {
+
+    Vector3 realPosition = Vector3.zero;
+    Quaternion realRotation = Quaternion.identity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (photonView.isMine)
+        {
+
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, realPosition, 0.1f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, realRotation, 0.1f);
+        }
+	}
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            realPosition = (Vector3)stream.RecieveNext();
+            realRotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
